303304d6938dbb81b1d817a65246ee985dfe3b79
Osorio Malache <osorio.malache@inage.gov.mz>
2023-11-18 23:11:18 +0200

    dockerized

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // API_BASE_URL: 'http://localhost:8080',
-  API_BASE_URL: 'http://192.168.1.3:8080',
+  API_BASE_URL: 'http://localhost:8080',
+  // API_BASE_URL: 'http://41.94.38.139:8080',
   GPJ_VERSION: '01.00.00'
 };
